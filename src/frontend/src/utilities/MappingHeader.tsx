4d2fa06578752f48998e83460d10a14320f336c2
Laurent Savaëte <laurent@where.tf>
2024-06-15 13:09:11 +0200

    Change header

--- src/frontend/src/utilities/MappingHeader.tsx
+++ src/frontend/src/utilities/MappingHeader.tsx
@@ -5,14 +5,14 @@ const MappingHeader = () => {
   return (
     <CoreModules.Box sx={{ display: 'flex', justifyContent: 'space-between', padding: '1rem', fontSize: '14px' }}>
       <CoreModules.Typography color="error" sx={{ fontSize: '14px' }}>
-        Mapping our world together
+        Sauvons la France (eh oui, rien que ça!)
       </CoreModules.Typography>
       <CoreModules.Link
-        to="https://www.hotosm.org/"
+        to="https://frontpopulaire-2024.fr"
         style={{ textDecoration: 'none', color: '#d73f3e' }}
         target="_blank"
       >
-        hotosm.org
+        frontpopulaire-2024.fr
       </CoreModules.Link>
     </CoreModules.Box>
   );
